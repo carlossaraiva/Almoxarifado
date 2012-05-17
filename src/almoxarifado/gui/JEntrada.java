15f1c6ab231a3cbba73ef288ef1f384f10524f39
Carlos Saraiva <carlos.saraiva@msn.com>
2012-05-16 23:05:44 -0300

    Adicionando classe Estoque

---
+++ src/almoxarifado/gui/JEntrada.java
@@ -0,0 +1,57 @@
+package almoxarifado.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+public class JEntrada extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtQtde;
+
+
+	public JEntrada() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 337, 181);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox cboProduto = new JComboBox();
+		cboProduto.setBounds(100, 11, 150, 20);
+		contentPane.add(cboProduto);
+		
+		JComboBox cboFornecedor = new JComboBox();
+		cboFornecedor.setBounds(100, 39, 150, 20);
+		contentPane.add(cboFornecedor);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(100, 114, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		txtQtde = new JTextField();
+		txtQtde.setBounds(100, 70, 86, 20);
+		contentPane.add(txtQtde);
+		txtQtde.setColumns(10);
+		
+		JLabel lblProduto = new JLabel("Produto:");
+		lblProduto.setBounds(10, 14, 46, 14);
+		contentPane.add(lblProduto);
+		
+		JLabel lblFornecedor = new JLabel("Fornecedor:");
+		lblFornecedor.setBounds(10, 42, 61, 14);
+		contentPane.add(lblFornecedor);
+		
+		JLabel lblQtde = new JLabel("Qtde.:");
+		lblQtde.setBounds(10, 73, 46, 14);
+		contentPane.add(lblQtde);
+	}
+}
--- src/almoxarifado/gui/JEstoque.java
+++ src/almoxarifado/gui/JEstoque.java
@@ -1,21 +1,88 @@
 package almoxarifado.gui;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTabbedPane;
+import javax.swing.JToolBar;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JSeparator;
 
-public class JEstoque extends JPanel {
+public class JEstoque extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-
+	private JTabbedPane tab;
+	private JPanel painelEstoque;
+	private JFrame caixaEntrada;
+	private JEntrada entrada;
+	
 	public JEstoque() {
 		setLayout(null);
+		painelEstoque = new JPanel();
+				
+		tab = new JTabbedPane(JTabbedPane.TOP);
+		tab.setBounds(0, 0, 667, 476);
+		tab.addTab("Estoque", painelEstoque);
+		painelEstoque.setLayout(null);
 		
-		JButton btnGerarSada = new JButton("Gerar Sa\u00EDda");
-		btnGerarSada.setBounds(44, 391, 117, 29);
-		add(btnGerarSada);
+		JLabel lblDatahora = new JLabel("Data/Hora:");
+		lblDatahora.setBounds(21, 32, 59, 14);
+		painelEstoque.add(lblDatahora);
+		
+		JLabel lbldataHoraAqui = new JLabel("(Data HOra Aqui");
+		lbldataHoraAqui.setBounds(90, 32, 79, 14);
+		painelEstoque.add(lbldataHoraAqui);
+		
+		JButton btnNovaEntrada = new JButton("Nova Entrada");
+		btnNovaEntrada.addActionListener(this);
+		
+		btnNovaEntrada.setBounds(23, 380, 113, 23);
+		painelEstoque.add(btnNovaEntrada);
+		
+		JButton btnNovaSada = new JButton("Nova Sa\u00EDda");
+		btnNovaSada.setBounds(146, 380, 105, 23);
+		painelEstoque.add(btnNovaSada);
+		
+		JLabel lblltimasEntradas = new JLabel("\u00DAltimas entradas:");
+		lblltimasEntradas.setBounds(21, 98, 96, 14);
+		painelEstoque.add(lblltimasEntradas);
+		
+		JLabel lblltimasSadas = new JLabel("\u00DAltimas sa\u00EDdas:");
+		lblltimasSadas.setBounds(21, 169, 96, 14);
+		painelEstoque.add(lblltimasSadas);
+		
+		JLabel lblProdutoEmEstoque = new JLabel("Produto em Estoque M\u00EDnimo");
+		lblProdutoEmEstoque.setBounds(21, 232, 148, 14);
+		painelEstoque.add(lblProdutoEmEstoque);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(21, 150, 631, 14);
+		painelEstoque.add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(21, 73, 631, 14);
+		painelEstoque.add(separator_1);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setBounds(21, 207, 631, 14);
+		painelEstoque.add(separator_2);
+		
+		JSeparator separator_3 = new JSeparator();
+		separator_3.setBounds(21, 281, 631, 14);
+		painelEstoque.add(separator_3);
+		add(tab);
 
+		entrada = new JEntrada();
+		
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		entrada.setVisible(true);
 	}
 }
